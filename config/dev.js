055023809d6cc9a5f8b198c40c64f2d072390c2c
barneslardo <barneslardo@gmail.com>
2019-05-03 12:30:21 -0700

    add config/dev/js file

---
+++ config/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mongoURI: "mongodb://skylar:my556AR12@ds161794.mlab.com:61794/k12passport",
+  googleClientID:
+    "267327884383-sale8722b11t43o9cc0b38gu7lm465dc.apps.googleusercontent.com",
+  googleClientSecret: "uqHXl_4jYeuG72iX8qAkCI4t",
+  cookieKey: "oq78gfq87gf348gf83g3o4gfqwjhfsjdgfjhsgfuf47g8w",
+  oktaToken: "0oaf3d8zj7Kqphzzo356",
+  issuer: "https://bigfootwebservice.okta.com/oauth2/default",
+  clientID: "0oaf3d8zj7Kqphzzo356",
+  clientSECRET: "FL0vnyk93Wl6BggCU3wfvADx9edP03J4VPEJlhLr",
+  callbackURL: "http://localhost:5000/auth/oktaOIDC/callback",
+  scope: "openid profile"
+};
